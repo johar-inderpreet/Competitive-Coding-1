a5665efc5793c5c478991de855aa5f7cfa9ca372
Inderpreet Johar <johar.inderpreet@gmail.com>
2025-08-24 12:49:49 -0500

    Mock Interview: Find Missing Number

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,28 @@
+//Time Complexity: O(logn)
+//Space Complexity: O(1)
+//Approach: Check if the middle element == mid + 1 (which would mean that the left side does not have missing elements)
+//in that case, search on the right side, else search on the right side
+//finally, the while loop will end because low will cross over the high, and the output will be low + 1 or arr[low] - 1
+class Solution {
 
+    /**
+     * Finds the missing number in the array using binary search
+     *
+     * @param arr the array
+     * @return    the missing number in the array
+     */
+    public int missingNumber(int arr[]) {
+
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == mid + 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low + 1;
+    }
+}
